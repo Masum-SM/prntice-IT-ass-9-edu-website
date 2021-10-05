9aa93ef77c6fb830fb5efbe74293761c7fec384e
Masum-SMe <unusmasum0@gmail.comm>
2021-10-05 20:07:12 +0600

    update home file

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -12,26 +12,33 @@ const Home = () => {
             .then(data => setCourses(data))
     }, [])
     return (
-        <div className='home-body'>
-            <div className='title-div'>
-                <h2 className="title">Welcome to <span className='Prentice'>Prentice IT</span> Institute: Where IT Leaders are Created</h2>
-                <h4>Prentice IT Institute, one of the leading IT training institutes in Bangladesh offers the best training opportunities. It has been playing a vital role to eradicate the unemployment problem since 2008. Enriched with quality training this institute has never failed to help the individuals to reveal their talents making harmony between creativity and IT. No matter what is your background, we are offering 30 courses for you conducted by experienced trainers to advance your skills.</h4>
+        <div>
+            <div className='image'>
+                <img src="./sb.jpg" alt="" />
             </div>
+            <div className='home-body'>
+
+                <div className='title-div'>
+                    <h2 className="title">Welcome to <span className='Prentice'>Prentice IT</span> Institute: Where IT Leaders are Created</h2>
+                    <h4>Prentice IT Institute, one of the leading IT training institutes in Bangladesh offers the best training opportunities. It has been playing a vital role to eradicate the unemployment problem since 2008. Enriched with quality training this institute has never failed to help the individuals to reveal their talents making harmony between creativity and IT. No matter what is your background, we are offering 30 courses for you conducted by experienced trainers to advance your skills.</h4>
+                </div>
 
 
-            <Row xs={1} md={2} className="g-4">
-                {
-                    courses.map(course => <CartHome
-                        key={course.code}
-                        course={course}
-                    ></CartHome>)
-                }
+                <Row xs={1} md={2} className="g-4">
+                    {
+                        courses.map(course => <CartHome
+                            key={course.code}
+                            course={course}
+                        ></CartHome>)
+                    }
 
-            </Row>
+                </Row>
 
 
 
+            </div>
         </div>
+
     );
 };
 
